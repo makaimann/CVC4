b02a44001d6b9676b99401153999286119a4ff90
Mathias Preiner <mathias.preiner@gmail.com>
2018-05-01 10:10:08 -0700

    Format.

--- src/options/options_handler.cpp
+++ src/options/options_handler.cpp
@@ -1103,7 +1103,7 @@ theory::bv::SatSolverMode OptionsHandler::stringToSatSolver(std::string option,
       throw OptionException(
           std::string("CaDiCaL does not support incremental mode. \n\
                          Try --bv-sat-solver=cryptominisat or "
-                       "--bv-sat-solver=minisat"));
+                      "--bv-sat-solver=minisat"));
     }
 
     if (options::bitblastMode() == theory::bv::BITBLAST_MODE_LAZY
