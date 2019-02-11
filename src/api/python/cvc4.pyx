ef4e286c087f6e815eb12bdbfab64b33f7c419fd
Makai Mann <makaim@stanford.edu>
2019-02-11 10:36:02 -0800

    Define __BUILDING_CVC4LIB

--- src/api/python/cvc4.pyx
+++ src/api/python/cvc4.pyx
@@ -1,6 +1,6 @@
 # distutils: language = c++
 # distutils: include_dirs = ../ ../include/
-# distutils: extra_compile_args = -std=c++11
+# distutils: extra_compile_args = -std=c++11 -D__BUILDING_CVC4LIB
 # distutils: libraries = cvc4 cvc4parser
 
 from collections import Sequence
